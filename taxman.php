be8d03cee29ff6a95efc1577a47aaf1adb34187c
Ryan Van Etten <ryanve@gmail.com>
2013-05-26 16:33:19 -0400

    0.1.2: Handle checks outside the loop.

--- taxman.php
+++ taxman.php
@@ -1,10 +1,9 @@
 <?php
-
 /*
 Plugin Name: Taxman
 Plugin URI: http://github.com/ryanve/taxman
 Description: Extend existing taxonomies to other post types.
-Version: 0.1.1
+Version: 0.1.2
 Author: Ryan Van Etten
 Author URI: http://ryanve.com
 License: MIT
@@ -13,9 +12,14 @@ License: MIT
 add_action('init', function() {
     # Core types include: post page attachment revision nav_menu_item
     # Consider: get_post_types, get_taxonomies, get_object_taxonomies
+    $types = apply_filters('plugin:taxman:types', array('page'));
     $taxos = apply_filters('plugin:taxman:taxos', array('post_tag', 'category'));
-    $types = apply_filters('plugin:taxman:types', array('page')); # applicable post types
-    foreach (array_filter($types, 'post_type_exists') as $type)
+    if ( ! $types || ! $taxos)
+        return;
+    # Do redundant checks to be safe.
+    $types = array_filter($types, 'post_type_exists');
+    $taxos = array_filter($taxos, 'taxonomy_exists');
+    foreach ($types as $type)
         foreach ($taxos as $tax)
-            taxonomy_exists($tax) and register_taxonomy_for_object_type($tax, $type);
+            register_taxonomy_for_object_type($tax, $type);
 }, 100); # Run after custom type/taxo registrations.
